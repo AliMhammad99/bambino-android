4babb5b8fe9c3b59eb9b16ca9b9300ecd42e381e
Ali Mhammad <alimhmd.engineer@gmail.com>
2023-03-25 07:21:06 +0200

    Working on navigation

--- app/src/main/java/com/fyp/bambino/MainActivity.java
+++ app/src/main/java/com/fyp/bambino/MainActivity.java
@@ -3,13 +3,39 @@ package com.fyp.bambino;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageButton buttonDashBoard;
+    private ImageButton buttonLiveVideo;
+    private ImageButton buttonConfig;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
+
+        buttonDashBoard = findViewById(R.id.btn_dashboard);
+        buttonLiveVideo = findViewById(R.id.btn_live_video);
+        buttonConfig = findViewById(R.id.btn_config);
+        setupNavButton(buttonDashBoard);
+        setupNavButton(buttonLiveVideo);
+        setupNavButton(buttonConfig);
+
+    }
+
+    private void setupNavButton(ImageButton navButton){
+        navButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                view.setFocusable(true);
+                view.setFocusableInTouchMode(true);
+                view.requestFocus();
+            }
+        });
     }
 }
