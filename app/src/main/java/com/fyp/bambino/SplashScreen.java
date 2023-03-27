44e3278a5646165ab3cc2c2c17d6e5a3c0dd5db6
Ali Mhammad <alimhmd.engineer@gmail.com>
2023-03-27 08:24:56 +0300

    Local Live Demo DONE

--- app/src/main/java/com/fyp/bambino/SplashScreen.java
+++ app/src/main/java/com/fyp/bambino/SplashScreen.java
@@ -10,11 +10,15 @@ import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Matrix;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
+import android.os.HandlerThread;
 import android.os.Looper;
+import android.os.Message;
 import android.util.Base64;
 import android.util.Log;
 import android.view.SurfaceView;
+import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.CompoundButton;
@@ -32,12 +36,16 @@ import com.google.firebase.database.ValueEventListener;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 import okhttp3.Call;
@@ -49,81 +57,175 @@ import okhttp3.Response;
 public class SplashScreen extends AppCompatActivity {
 
     private ImageView imageView;
-    private OkHttpClient client;
-    private Request request;
-    private String url;
-    private String partBoundary = "123456789000000000000987654321";
+    private HandlerThread stream_thread,flash_thread,rssi_thread;
+    private Handler stream_handler,flash_handler,rssi_handler;
 
+    private final int ID_CONNECT = 200;
+    private final int ID_FLASH = 201;
+    private final int ID_RSSI = 202;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
         imageView = findViewById(R.id.imageView);
-        // Get the SurfaceView component from the layout
-        client = new OkHttpClient();
-        url = "http://192.168.43.239/";
-        request = new Request.Builder()
-                .url(url)
-                .addHeader("Content-Type", "multipart/x-mixed-replace;boundary=" + partBoundary)
-                .build();
-        Callback callback = new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
-                e.printStackTrace();
-            }
 
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
-                if (!response.isSuccessful()) {
-                    throw new IOException("Unexpected code " + response);
-                }
-
-                // Parse the response and update the ImageView
-                InputStream inputStream = response.body().byteStream();
-                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    if (line.startsWith("--" + partBoundary)) {
-                        // Extract the image data and update the ImageView
-                        Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                imageView.setImageBitmap(bitmap);
-                            }
-                        });
-                    }
-                }
-            }
-        };
-
-// Start the HTTP request to receive the streamed images
-        client.newCall(request).enqueue(callback);
+        stream_thread = new HandlerThread("http");
+        stream_thread.start();
+        stream_handler = new HttpHandler(stream_thread.getLooper());
+        stream_handler.sendEmptyMessage(ID_CONNECT);
     }
 
-
-//    private class UpdateSurfaceView implements Runnable {
-//        private Bitmap frame;
-//
-//        public UpdateSurfaceView(Bitmap frame) {
-//            this.frame = frame;
-//        }
-//
-//        @Override
-//        public void run() {
-//            // Get the Canvas object from the SurfaceView's SurfaceHolder
-//            Canvas canvas = surfaceView.getHolder().lockCanvas();
-//
-//            if (canvas != null) {
-//                try {
-//                    // Draw the new frame onto the Canvas
-//                    canvas.drawBitmap(frame, 0, 0, null);
-//                } finally {
-//                    // Unlock the Canvas and update the SurfaceView
-//                    surfaceView.getHolder().unlockCanvasAndPost(canvas);
-//                }
-//            }
+//    @Override
+//    public void onClick(View v)
+//    {
+//        switch (v.getId())
+//        {
+//            case R.id.connect:
+//                stream_handler.sendEmptyMessage(ID_CONNECT);
+//                rssi_handler.sendEmptyMessage(ID_RSSI);
+//                break;
+//            case R.id.flash:
+//                flash_handler.sendEmptyMessage(ID_FLASH);
+//                break;
+//            default:
+//                break;
 //        }
 //    }
+    private class HttpHandler extends Handler
+    {
+        public HttpHandler(Looper looper)
+        {
+            super(looper);
+        }
 
+        @Override
+        public void handleMessage(Message msg)
+        {
+            switch (msg.what)
+            {
+                case ID_CONNECT:
+                    VideoStream();
+                    break;
+//                case ID_FLASH:
+//                    SetFlash();
+//                    break;
+//                case ID_RSSI:
+//                    GetRSSI();
+//                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    private void VideoStream()
+    {
+        String stream_url = "http://192.168.43.239:80";
+
+        BufferedInputStream bis = null;
+        FileOutputStream fos = null;
+        try
+        {
+
+            URL url = new URL(stream_url);
+
+            try
+            {
+                HttpURLConnection huc = (HttpURLConnection) url.openConnection();
+                huc.setRequestMethod("GET");
+                huc.setConnectTimeout(1000 * 5);
+                huc.setReadTimeout(1000 * 5);
+                huc.setDoInput(true);
+                huc.connect();
+
+                if (huc.getResponseCode() == 200)
+                {
+                    InputStream in = huc.getInputStream();
+
+                    InputStreamReader isr = new InputStreamReader(in);
+                    BufferedReader br = new BufferedReader(isr);
+
+                    String data;
+
+                    int len;
+                    byte[] buffer;
+
+                    while ((data = br.readLine()) != null)
+                    {
+                        if (data.contains("Content-Type:"))
+                        {
+                            data = br.readLine();
+
+                            len = Integer.parseInt(data.split(":")[1].trim());
+
+                            bis = new BufferedInputStream(in);
+                            buffer = new byte[len];
+
+                            int t = 0;
+                            while (t < len)
+                            {
+                                t += bis.read(buffer, t, len - t);
+                            }
+
+                            Bytes2ImageFile(buffer, getExternalFilesDir(Environment.DIRECTORY_PICTURES) + "/0A.jpg");
+
+                            final Bitmap bitmap = BitmapFactory.decodeFile(getExternalFilesDir(Environment.DIRECTORY_PICTURES) + "/0A.jpg");
+
+                            runOnUiThread(new Runnable()
+                            {
+                                @Override
+                                public void run()
+                                {
+                                    imageView.setImageBitmap(bitmap);
+                                }
+                            });
+
+                        }
+
+
+                    }
+                }
+
+            } catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        } catch (MalformedURLException e)
+        {
+            e.printStackTrace();
+        } finally
+        {
+            try
+            {
+                if (bis != null)
+                {
+                    bis.close();
+                }
+                if (fos != null)
+                {
+                    fos.close();
+                }
+
+                stream_handler.sendEmptyMessageDelayed(ID_CONNECT,3000);
+            } catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+
+    }
+    private void Bytes2ImageFile(byte[] bytes, String fileName)
+    {
+        try
+        {
+            File file = new File(fileName);
+            FileOutputStream fos = new FileOutputStream(file);
+            fos.write(bytes, 0, bytes.length);
+            fos.flush();
+            fos.close();
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
 }
