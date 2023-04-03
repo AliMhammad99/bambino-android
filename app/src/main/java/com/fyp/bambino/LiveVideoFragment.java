5c675f776be9f157cb4762e4465ef0226f3410a1
Ali Mhammad <alimhmd.engineer@gmail.com>
2023-04-03 17:57:31 +0300

    Live Video UI Done

--- app/src/main/java/com/fyp/bambino/LiveVideoFragment.java
+++ app/src/main/java/com/fyp/bambino/LiveVideoFragment.java
@@ -9,7 +9,6 @@ import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.os.Handler;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
